b05ce4b052cc102632ca6d49fd9a765323563408
oleja_kosmolet <olegermolaev45@gmail.com>
2023-09-16 16:45:00 +0300

    ded masha

---
+++ examples005_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else 
+{
+    Console.WriteLine($"Привет, {userName} ");
+}

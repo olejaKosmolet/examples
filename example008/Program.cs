db9426a7dbf51c31d23b2f5e206f6629a613989f
oleja_kosmolet <olegermolaev45@gmail.com>
2023-09-18 19:33:19 +0300

    added dog shit

---
+++ example008/Program.cs
@@ -0,0 +1,25 @@
+﻿int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int count = 0;
+int time;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance/(secondFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+System.Console.WriteLine($"Собака пробежала {count} раз.");
